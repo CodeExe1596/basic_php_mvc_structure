2c2fd7e2286f8209556b23ba32b38bc6337e83ee
Amine <code.exe1596@gmail.com>
2021-07-19 23:52:03 +0100

    Adding About Controller

---
+++ controllers/about_Controller.php
@@ -0,0 +1,25 @@
+<?php 
+
+    /**
+     * About Controller
+     */
+    class About_Controller extends Controller 
+    {
+
+        /**
+         * Constructor
+         */
+        public function __construct()
+        {
+            $this->_view_file = 'about';
+        }
+
+        /**
+         * About
+         * Index action
+         */
+        public function index()
+        {
+
+        }
+    }
