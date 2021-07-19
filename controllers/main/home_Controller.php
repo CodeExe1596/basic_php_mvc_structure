7975895ded391deb4a7f490fce893c9790ec63af
Amine <code.exe1596@gmail.com>
2021-07-19 23:18:05 +0100

    Addding Controllers folder with main default home Controller

---
+++ controllers/main/home_Controller.php
@@ -0,0 +1,26 @@
+<?php 
+
+    /**
+     * Home Controller class 
+     */
+    class Home_Controller extends Controller 
+    {
+        /**
+         * Constructor
+         */
+        public function __construct()
+        {
+            $this->_view_file = 'home';
+        }
+
+        /**
+         * public Function 
+         * Default index action of home controller
+         * Display main 'home' page
+         */
+        public function index()
+        {
+            
+        }
+
+    }
