bb3f71f856167882e779a5ea43690cca83cb3ac8
Amine <code.exe1596@gmail.com>
2021-07-19 23:40:09 +0100

    Adding views folder with main home view

---
+++ views/home/index_view.php
@@ -0,0 +1,15 @@
+<!-- php Entities includes -->
+<?php require_once(ROOT . 'models/layouts/user.php'); ?>
+
+<!-- View Cascading Stylesheet -->
+<style>
+
+
+
+</style>
+
+<!-- View -->
+<main>
+
+
+</main>
